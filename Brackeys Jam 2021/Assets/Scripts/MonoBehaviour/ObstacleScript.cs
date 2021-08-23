d015679e1df6a7df4c4c7bac73dbf7e559a815bb
Lionel Miele-Herndon <miell534@newschool.edu>
2021-08-23 14:47:16 -0400

    player script

---
+++ Brackeys Jam 2021/Assets/Scripts/MonoBehaviour/ObstacleScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleScript : Obstacle
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Brackeys Jam 2021/Assets/Scripts/MonoBehaviour/PlayerScript.cs
+++ Brackeys Jam 2021/Assets/Scripts/MonoBehaviour/PlayerScript.cs
@@ -6,6 +6,14 @@ public class PlayerScript : MonoBehaviour
 {
     //put movement and visual scripting in here
 
-    float speed;
-    Vector3 direction, velocity;
+    float baseSpeed = 2.0f;
+    Vector3 velocity;
+    [SerializeField] private Rigidbody2D thisBody;
+
+    void Update() {
+        velocity = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+
+        thisBody.velocity = velocity * baseSpeed;
+        // thisBody.AddForce(velocity * baseSpeed, ForceMode2D.Force);
+    }
 }

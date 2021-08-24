dbd344af22dace201f265ee57c425ade475a78b0
TheDarkBatman <87569239+TheDarkBatman2@users.noreply.github.com>
2021-08-24 22:41:51 +0430

    Added basic main menu scene
    main menu scene with options menu and button placeholders

---
+++ Brackeys Jam 2021/Assets/Scripts/Managers/StartMenuUiManager.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class StartMenuUiManager : MonoBehaviour
+{
+    public int GameSceneIndex;
+    public void LoadGameScene() {
+        SceneManager.LoadScene(GameSceneIndex);
+    }
+    public void QuitGame() {
+        Application.Quit();
+    }
+}
